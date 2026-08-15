d98cdcfb2e42bf1c08f991aa574b4c3c26fe0b78
ItsN3M3515 <pablotago@unisabana.edu.co>
2026-08-15 00:12:35 -0500

    avance tarea

--- src/test/java/edu/unisabana/dyas/patterns/util/LengthFilterTest.java
+++ src/test/java/edu/unisabana/dyas/patterns/util/LengthFilterTest.java
@@ -1,13 +1,12 @@
 package edu.unisabana.dyas.patterns.util;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class LengthFilterTest {
 
